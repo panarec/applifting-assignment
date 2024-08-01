f1d984a85601f434661bd3e5a3e3f6be4c156ed3
Pavol Vernarec <pver@paymorrow.com>
2024-08-01 09:05:49 +0200

    graphql comments added

--- src/graphql.ts
+++ src/graphql.ts
@@ -1,3 +1,4 @@
+
 /*
  * -------------------------------------------------------
  * THIS FILE WAS AUTOMATICALLY GENERATED (DO NOT MODIFY)
@@ -8,42 +9,38 @@
 /* eslint-disable */
 
 export interface CreateCommentInput {
-  content: string;
-  postId: number;
+    content: string;
+    postId: number;
 }
 
 export interface DeleteCommentInput {
-  commentId: number;
+    commentId: number;
 }
 
 export interface UpdateCommentInput {
-  commentId: number;
-  content: string;
+    commentId: number;
+    content: string;
 }
 
 export interface Comment {
-  authorId: number;
-  content: string;
-  createdAt: string;
-  id: number;
-  postId: number;
-  updatedAt: string;
-  votes: number;
+    authorId: number;
+    content: string;
+    createdAt: string;
+    id: number;
+    postId: number;
+    updatedAt: string;
+    votes: number;
 }
 
 export interface IMutation {
-  createComment(
-    createCommentInput?: Nullable<CreateCommentInput>,
-  ): Nullable<Comment> | Promise<Nullable<Comment>>;
-  deleteComment(id: number): Nullable<Comment> | Promise<Nullable<Comment>>;
-  updateComment(
-    updateCommentInput?: Nullable<UpdateCommentInput>,
-  ): Nullable<Comment> | Promise<Nullable<Comment>>;
+    createComment(createCommentInput?: Nullable<CreateCommentInput>): Nullable<Comment> | Promise<Nullable<Comment>>;
+    deleteComment(id: number): Nullable<Comment> | Promise<Nullable<Comment>>;
+    updateComment(updateCommentInput?: Nullable<UpdateCommentInput>): Nullable<Comment> | Promise<Nullable<Comment>>;
 }
 
 export interface IQuery {
-  comment(id: number): Comment | Promise<Comment>;
-  comments(): Comment[] | Promise<Comment[]>;
+    comment(id: number): Comment | Promise<Comment>;
+    comments(): Comment[] | Promise<Comment[]>;
 }
 
 type Nullable<T> = T | null;
